81ccd9ee220a285955e19720b6526eb3aea8d2ba
Mohit Juneja <78297119+Mohit-Juneja-prog@users.noreply.github.com>
2022-08-26 15:30:33 +0530

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,53 @@
+// Mohit Juneja
+// 2010991631
+// CU Coding-Test 04
+// Question: 1
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int findMinIndex(int arr[], int n)
+{
+    int minIndex = n;
+ 
+    unordered_set<int> set;
+ 
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (set.find(arr[i]) != set.end()) {
+            minIndex = i;
+        }
+        else {
+            set.insert(arr[i]);
+        }
+    }
+ 
+    if (minIndex == n) {
+        return -1;
+    }
+ 
+    return minIndex;
+}
+ 
+int main()
+{
+    int k;
+    cin>>k;
+    int arr[k];
+    for(int i=0;i<k;i++){
+    cin>>arr[i];  }
+ 
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    int minIndex = findMinIndex(arr, n);
+ 
+    if (minIndex != n-1) {
+        cout << "The minimum index of the repeating element is " << minIndex;
+    }
+    else {
+        cout << "Invalid Input";
+    }
+ 
+    return 0;
+}
---
+++ Q2.cpp
@@ -0,0 +1,60 @@
+// Mohit Juneja
+// 2010991631
+// CU Coding-Test 04
+// Question: 2
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+ 
+bool isConsecutive(int arr[], int n)
+{
+    if (n <= 1) {
+        return true;
+    }
+ 
+    int min = arr[0], max = arr[0];
+ 
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+ 
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+ 
+    if (max - min != n - 1) {
+        return false;
+    }
+ 
+    unordered_set<int> visited;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (visited.find(arr[i]) != visited.end()) {
+            return false;
+        }
+ 
+        visited.insert(arr[i]);
+    }
+ 
+    return true;
+}
+ 
+int main()
+{
+    int k;
+    cin>>k;
+    int arr[k];
+    for(int i=0;i<k;i++){
+    cin>>arr[i];  }
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    isConsecutive(arr, n)? cout << "The array contains consecutive integers":
+                        cout << "The array does not contain consecutive integers";
+ 
+    return 0;
+}
